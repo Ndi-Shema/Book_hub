cd58616bddee1c91407cb53118c262eb13203574
Ndi-Shema <f.shema@alustudent.com>
2024-08-01 19:31:25 +0200

    modifying app.tsx

--- book-hub/src/App.tsx
+++ book-hub/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BookList from './components/BookList';
+import BookDetail from './components/BookDetail';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Book Hub</h1>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/" exact component={BookList} />
+            <Route path="/book/:id" component={BookDetail} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
